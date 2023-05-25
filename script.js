fb37a4bad8c9c9d8aebc5d21ef1dff506ab59d4a
catrinekjaergaard <114469237+catrinekjaergaard@users.noreply.github.com>
2023-05-25 21:11:08 +0200

    små ting
    header lukke, video rettelser, stavefejl, ekstra tlf osv

--- script.js
+++ script.js
@@ -3,15 +3,21 @@
 const burger = document.querySelector(".fa-bars");
 const overlay = document.querySelector(".mobil-menu");
 const x = document.querySelector(".fa-x");
+const menuluk = document.querySelector(".mobil-menu"); 
 
 burger.addEventListener("click", () => {
-  overlay.style.height = "100%";
+  overlay.style.height = "100%"; // Åbner menuen når man har klikket på burgeren
 });
 
 x.addEventListener("click", () => {
-  overlay.style.height = "0";
+  overlay.style.height = "0"; // lukker menuen når man har klikket på krydset
 });
 
+menuluk.addEventListener("click", () => {
+    overlay.style.height = "0";
+  }); // lukker menuen når man har klikket på et af menu punkterne
+
+// har ikke nåede at lave min header sticky, men det skulle den selvfølgelig være
 
 //Slideshow
 const slideshowImages = document.querySelectorAll(".billederslideshow"); //vælger alel billederne slideshow div
@@ -24,10 +30,10 @@ slideshowImages[imageCounter].style.display = "block"; //Blokere de billeder der
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
-slideshowImages[imageCounter].style.display = "none";
-imageCounter = (imageCounter + 1) % slideshowImages.length; //viser billede og lægger det næste ovn i og length gør at den bare fortsætter uden at stoppe
-slideshowImages[imageCounter].style.display = "block";
-}
+    slideshowImages[imageCounter].style.display = "none";
+    imageCounter = (imageCounter + 1) % slideshowImages.length; //viser billede og lægger det næste ovn i og length gør at den bare fortsætter uden at stoppe
+    slideshowImages[imageCounter].style.display = "block";
+ }
 
 
 
